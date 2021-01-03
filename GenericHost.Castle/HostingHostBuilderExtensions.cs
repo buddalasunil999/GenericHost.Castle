68938d02206e91fcdc900bfe5dfe5cf24f2db3cf
Sunil Buddala <sunil@donatekart.com>
2021-01-04 11:54:48 +1300

    added log4net

---
+++ GenericHost.Castle/HostingHostBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using log4net.Config;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+
+namespace GenericHost.Castle
+{
+    public static class HostingHostBuilderExtensions
+    {
+        public static IHostBuilder ConfigureLog4Net(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureServices((context, collection) =>
+            {
+                var fileName = context.HostingEnvironment.IsDevelopment() ? "log4net.config" :
+                $"log4net.{context.HostingEnvironment.EnvironmentName}.config";
+
+                var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+                XmlConfigurator.Configure(logRepository, new FileInfo(fileName));
+            });
+        }
+    }
+}
